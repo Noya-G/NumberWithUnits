a681f042f0bf6433d5e1de0b0fbb398706a76885
Noya-G <73538626+Noya-G@users.noreply.github.com>
2021-04-20 20:32:45 +0300

    Create NumberWithUnits.hpp

---
+++ NumberWithUnits.hpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <iostream>
+
+namespace ariel{
+    class NumberWithUnits
+    {
+    private:
+        std::string unit;
+        double num;
+    public:
+        NumberWithUnits(std::string unit);
+        NumberWithUnits(double num, std::string unit);
+        ~NumberWithUnits();
+
+        //===============================================================
+        //Arithmetic Operators===========================================
+        NumberWithUnits& operator+ (const NumberWithUnits& nwu);
+        NumberWithUnits& operator+= (const NumberWithUnits& nwu);
+        NumberWithUnits& operator+ ();
+        NumberWithUnits& operator- (const NumberWithUnits& nwu);
+        NumberWithUnits& operator-= (const NumberWithUnits& nwu);
+        NumberWithUnits& operator- () ;
+        //===============================================================
+
+
+        //===============================================================
+        //Compartion Operators===========================================
+        bool operator< (const NumberWithUnits& nwu);
+        bool operator<= (const NumberWithUnits& nwu);
+        bool operator> (const NumberWithUnits& nwu);
+        bool operator>= (const NumberWithUnits& nwu);
+        bool operator== (const NumberWithUnits& nwu);
+        bool operator!= (const NumberWithUnits& nwu);
+        //===============================================================
+
+
+        //===============================================================
+        //Prefix Operators===============================================
+        NumberWithUnits& operator-- ();
+        NumberWithUnits& operator++ ();
+        //==============================================================
+
+
+        //==============================================================
+        //Psotfix Operators=============================================
+        NumberWithUnits& operator++ (int);
+        NumberWithUnits& operator++ (int);
+        //==============================================================
+
+
+        //==============================================================
+        //Multiplication Operators======================================
+        friend NumberWithUnits& operator* (NumberWithUnits& nwu, double num); 
+        friend NumberWithUnits& operator* (double num1, NumberWithUnits& nwu);
+        //==============================================================
+
+
+        //==============================================================
+        //Stream Operators==============================================
+        std::ostream operator<< (std::ostream& os);
+        std::istream operator>> (std::istream& is);
+        //==============================================================
+    };
+}
